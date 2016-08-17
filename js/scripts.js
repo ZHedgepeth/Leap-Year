4650d05d4879ab6275e051af76e3bc248e054d95
Anand Angalig and Zach Hedgepeth <anand@>
2016-08-17 10:45:56 -0400

    finish writing the initial version of the Business Logic with if statement fo each behavior

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 var leapYear = function(yearInput) {
-  if (yearInput % 4 === 0) {
+  if (yearInput % 400 === 0) {
+    return true;
+  } else if (yearInput % 100 === 0) {
+    return false;
+} else if (yearInput % 4 === 0) {
     return true;
   } else {
   return false;
